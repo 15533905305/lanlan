4d78e4b7ccfb0129c986ff77653a304bdee25021
wjl <1547913526@qq.com>
2018-09-16 11:51:30 +0800

    初始化项目

---
+++ src/main/java/edu/wjl/lanlan/LanlanApplication.java
@@ -0,0 +1,12 @@
+package edu.wjl.lanlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanlanApplication.class, args);
+    }
+}
---
+++ src/main/java/edu/wjl/lanlan/constants/FilterConditionConstants.java
@@ -0,0 +1,48 @@
+package edu.wjl.lanlan.constants;
+
+/**
+ * 过滤条件常量
+ *
+ * @author junli wang
+ * @date 18-9-16
+ */
+
+public class FilterConditionConstants {
+
+    public static Integer AGE_NINETEEN = 19;
+    public static Integer AGE_TWENTY_FIVE = 25;
+    public static Integer AGE_THIRTY = 30;
+    public static Integer AGE_THIRTY_FIVE = 35;
+    public static Integer AGE_FORTY = 40;
+    public static Integer AGE_FORTY_FIVE = 45;
+
+
+    public static String HOUSE_HAVE = "有房";
+    public static String HOUSE_UNKNOWN = "未知";
+    public static String HOUSE_NOT_REQUIRED = "/";
+
+
+    public static Integer ASSETTYPE_SPECIAL_HIGH =1;
+    public static String ASSETTYPE_HIGH = "高";
+    public static String ASSETTYPE_MIDDLE = "中";
+    public static String ASSETTYPE_LOW = "低";
+
+
+    public static String MARITALSTATUS_HAVE = "已婚";
+    public static String MARITALSTATUS_NOT_REQUIRED = "/";
+
+
+    public static String PROFESSION_STUDENT = "学生";
+    public static String PROFESSION_STAFF = "教职工";
+    public static String PROFESSION_NOT_REQUIRED = "/";
+
+    public static String TAG1 = "1";
+    public static String TAG2 = "2";
+    public static String TAG3 = "3";
+    public static String TAG4 = "4";
+
+
+
+
+
+}
